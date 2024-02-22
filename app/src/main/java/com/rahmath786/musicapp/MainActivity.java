3dd8c67bebdfc83582b8c2930bbc7e8fa7f97ed0
HP <rahmath786khan@gmail.com>
2024-02-22 18:33:21 +0530

    added the buttons and other details

--- app/src/main/java/com/rahmath786/musicapp/MainActivity.java
+++ app/src/main/java/com/rahmath786/musicapp/MainActivity.java
@@ -1,16 +1,65 @@
 package com.rahmath786.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingComponent;
+import androidx.databinding.DataBindingUtil;
 
+import android.database.DatabaseUtils;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.rahmath786.musicapp.databinding.ActivityMainBinding;
+
+import java.io.File;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding = null;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        initializeViews();
+        mediaPlayer = MediaPlayer.create(this, R.raw.sound1);
+        binding.musicTitle.setText("Sound1");
+
+
+        binding.playPause.setOnClickListener(v -> {
+            if (!mediaPlayer.isPlaying()) {
+                binding.playPause.setImageResource(R.drawable.pause_new);
+                mediaPlayer.start();
+                mediaPlayer.setLooping(true);
+            } else {
+                binding.playPause.setImageResource(R.drawable.play_new);
+                mediaPlayer.setLooping(false);
+                mediaPlayer.pause();
+            }
+        });
+
+        binding.nextSong.setOnClickListener(v -> {
+
+        });
+
+        binding.previousSong.setOnClickListener(v -> {
+
+        });
+    }
+
+    private void areAllPermissionsGranted() {
+        /** here we will check for the permissions required like
+         * read storage etc */
+
+
+    }
+
+    private void initializeViews() {
 
     }
 }
